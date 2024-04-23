e16bb2af115fe884020060429f49ffaf2f70fc05
Caetano Godinat <caetano.godinat@hotmail.com>
2024-04-23 13:55:05 +0900

    some tests and exercices with batch and epoch

---
+++ app/linearRegression/exampleTools.hs
@@ -0,0 +1,51 @@
+--hasktorch
+import Torch.Tensor (asValue)
+import Torch.Functional (mseLoss,add)
+import Torch.Device     (Device(..),DeviceType(..))
+import Torch.NN         (sample)
+import Torch.Optim      (GD(..))
+--hasktorch-tools
+import Torch.Tensor.TensorFactories (asTensor'')
+import Torch.Train      (update,showLoss,zeroTensor,saveParams,loadParams)
+import Torch.Control    (mapAccumM,foldLoop)
+import Torch.Layer.Linear (LinearHypParams(..),linearLayer)
+import ML.Exp.Chart (drawLearningCurve) --nlp-tools
+
+trainingData :: [([Float],Float)]
+trainingData = [([1],2),([2],4),([3],6),([1],2),([3],7)]
+
+testData :: [([Float],Float)]
+testData = [([3],7)]
+
+createModel :: Device -> LinearHypParams
+createModel device = LinearHypParams device True 1 1
+
+main :: IO()
+main = do
+  let iter = 500::Int
+      device = Device CPU 0
+  initModel <- sample $ createModel device
+  ((trainedModel,_),losses) <- mapAccumM [1..iter] (initModel,GD) $ \epoc (model,opt) -> do
+    let batchLoss = foldLoop trainingData zeroTensor $ \(input,output) loss ->
+                      let y' = linearLayer model $ asTensor'' device input
+                          y = asTensor'' device output
+                      in add loss $ mseLoss y y'
+        lossValue = (asValue batchLoss)::Float
+    showLoss 5 epoc lossValue
+    u <- update model opt batchLoss 5e-4
+    return (u, lossValue)
+  saveParams trainedModel "regression.model"
+  --mapM_ (putStr . printf "%2.3f ") $ reverse allLosses
+  drawLearningCurve "graph-reg.png" "Learning Curve" [("",reverse losses)]
+  loadedModel <- loadParams (createModel device) "regression.model"
+  print loadedModel
+  --
+  let output = linearLayer loadedModel $ asTensor'' device $ fst $ head testData
+      y' = (asValue output)::Float
+      y = snd $ head testData
+  putStr "\nPrediction: "
+  putStrLn $ show y'
+  putStr "Ground truth: "
+  putStrLn $ show y
+  putStr "Mse: "
+  putStrLn $ show $ (y' - y) * (y' - y)
