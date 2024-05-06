3d194db9b93d09c1e3f9a87b7fcd76123162cd5f
Caetano Godinat <caetano.godinat@hotmail.com>
2024-05-06 10:37:02 +0900

    working with adam!!

--- src/titanic-mlp/Titanic.hs
+++ src/titanic-mlp/Titanic.hs
@@ -15,8 +15,8 @@ import Control.Monad (when)
 import Data.List()
 import Torch.Tensor         (Tensor, asTensor, asValue)
 import Torch.Functional     (mseLoss)
-import Torch.NN             (sample)
-import Torch.Optim          (GD(..), foldLoop)
+import Torch.NN             (sample, flattenParameters)
+import Torch.Optim          (Adam(..), foldLoop, mkAdam)
 import Torch.Device         (Device(..),DeviceType(..))
 import Torch.Train          (update, saveParams, loadParams)
 import Torch.Layer.MLP (MLPHypParams(..), ActName(..), mlpLayer, MLPParams)
@@ -25,7 +25,6 @@ import Torch.Layer.MLP (MLPHypParams(..), ActName(..), mlpLayer, MLPParams)
 import System.IO.Unsafe (unsafePerformIO)
 import Data.List.Split (splitOn)
 import ML.Exp.Chart (drawLearningCurve) --nlp-tools
-import Torch (asValue, DType (Float))
 
 --------------------------------------------------------------------------------
 -- Data
@@ -58,18 +57,19 @@ titanic :: IO ()
 titanic = do
     init <- sample hyperParams
     -- init <- loadParams hyperParams "src/titanic-mlp/model-titanic.pt"
-
+    let opt = mkAdam itr beta1 beta2 (flattenParameters init)
     (trained, _, losses) <- foldLoop (init, opt, []) numIters $ \(model, optimizer, losses) i -> do
         let epochLoss = sum (map (loss model) trainingData)
         when (i `mod` 1 == 0) $ do
             print i
             print epochLoss
         (newState, newOpt) <- update model optimizer epochLoss lr
-        return (newState, newOpt, losses ++ [asValue epochLoss :: Float])
+        return (newState, newOpt, losses)
+        -- return (newState, newOpt, losses ++ [asValue epochLoss :: Float])
 
-    let filename = "src/titanic-mlp/curves/graph-titanic-mse" ++ show (last losses) ++ ".png"
-    let modelName = "src/titanic-mlp/models/model-titanic-" ++ show (last losses) ++ ".pt"
-    drawLearningCurve filename "Learning Curve" [("", losses)]
+    let filename = "src/titanic-mlp/curves/graph-titanic-mse" ++ show (0) ++ ".png"
+    let modelName = "src/titanic-mlp/models/model-titanic-" ++ show (0) ++ ".pt"
+    -- drawLearningCurve filename "Learning Curve" [("", losses)]
     saveParams trained modelName
 
     model <- loadParams hyperParams modelName
@@ -88,12 +88,15 @@ titanic = do
     return ()
 
     where
-        numIters = 60
-        lr = 0.0001
-        opt = GD
-        trainingData = parseDataTrain
+        numIters = 200
         device = Device CPU 0
         hyperParams = MLPHypParams device 7 [(21, Relu), (1, Id)]
+        -- betas are decaying factors Float, m's are the first and second moments [Tensor] and iter is the iteration number Int
+        itr = 0
+        beta1 = 0.9
+        beta2 = 0.999
+        lr = 1e-1
+        trainingData = parseDataTrain
 
 --------------------------------------------------------------------------------
 -- Validation
