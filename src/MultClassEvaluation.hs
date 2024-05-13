098b8492b80b316114b63cc89b54f2c5d4e5efb8
Caetano Godinat <caetano.godinat@hotmail.com>
2024-05-13 22:16:42 +0900

    removing IO abuses in MultClassEval

--- src/MultClassEvaluation.hs
+++ src/MultClassEvaluation.hs
@@ -1,105 +1,80 @@
-module MultClassEvaluation (accuracy, confusionMatrix, confusionMatrix', f1Macro, f1Micro, f1Weighted, fp, tp, fn, avarage, variance) where
+module MultClassEvaluation (accuracy, confusionMatrix, f1Macro, f1Micro, f1Weighted, fp, tp, fn, avarage, variance) where
 
 import Torch.Train        (loadParams)
 import Torch.Layer.MLP    (mlpLayer, MLPHypParams(..), MLPParams)
 import Torch.Tensor       (asValue, Tensor)
 import Func (argmax)
 
-tp :: [[Int]] -> Int -> IO Int
-tp confMat classIndex = do
-    return (confMat !! classIndex !! classIndex)
+tp :: [[Int]] -> Int -> Int
+tp confMat classIndex = confMat !! classIndex !! classIndex
 
-fp :: [[Int]] -> Int -> IO Int
-fp confMat classIndex = do
-    let numberOfClasses = length confMat
-    let row = confMat !! classIndex
-    return $ sum (map (row !!) [0..numberOfClasses-1]) - row !! classIndex
+fp :: [[Int]] -> Int -> Int
+fp confMat classIndex = sum (map (row !!) [0..numberOfClasses-1]) - row !! classIndex
+    where
+        numberOfClasses = length confMat
+        row = confMat !! classIndex
 
-fn :: [[Int]] -> Int -> IO Int
-fn confMat classIndex = do
-    let col = map (!! classIndex) confMat
-    return $ sum col - col !! classIndex
-
-
-accuracy :: [[Int]] -> IO Float
-accuracy confMat = do
-    let numberOfClasses = length confMat
-    tps <- mapM (tp confMat) [0..numberOfClasses-1]
-    let numberOfElements = sum (map sum confMat)
-    let accuracyValue = fromIntegral (sum tps) / fromIntegral numberOfElements
-    return accuracyValue
-
-confusionMatrix :: String -> [(Tensor, Tensor)] -> MLPHypParams -> IO [[Int]]
-confusionMatrix  modelPath dataset hyperParams = do
-    model <- loadParams hyperParams modelPath
-    let outputsTrain = map (\(input, actual) -> (actual, mlpLayer model input)) dataset
-    let outputsTrain' = map (\(actual, prediction) -> (actual, prediction)) outputsTrain
-    -- foreach (actual, prediction) in outputsTrain' actual is i and prediction is j
-    -- create a matrix of size 10x10
-    -- and add one to matrix[i][j]
-    let matrix = [[0 | _ <- [(0 :: Integer)..9]] | _ <- [(0 :: Integer)..9]]
-    let matrix' = foldl (\acc (actual, prediction) -> do
-            let i = argmax (asValue actual)
-            let j = argmax (asValue prediction)
-            let row = acc !! i
-            let row' = take j row ++ [row !! j + 1] ++ drop (j + 1) row
-            let acc' = take i acc ++ [row'] ++ drop (i + 1) acc
-            acc') matrix outputsTrain'
-    return matrix'
-
-confusionMatrix' :: MLPParams -> [(Tensor, Tensor)] -> IO [[Int]]
-confusionMatrix'  model dataset = do
-    let outputsTrain = map (\(input, actual) -> (actual, mlpLayer model input)) dataset
-    let outputsTrain' = map (\(actual, prediction) -> (actual, prediction)) outputsTrain
-    -- foreach (actual, prediction) in outputsTrain' actual is i and prediction is j
-    -- create a matrix of size 10x10
-    -- and add one to matrix[i][j]
-    let matrix = [[0 | _ <- [(0 :: Integer)..9]] | _ <- [(0 :: Integer)..9]]
-    let matrix' = foldl (\acc (actual, prediction) -> do
-            let i = argmax (asValue actual)
-            let j = argmax (asValue prediction)
-            let row = acc !! i
-            let row' = take j row ++ [row !! j + 1] ++ drop (j + 1) row
-            let acc' = take i acc ++ [row'] ++ drop (i + 1) acc
-            acc') matrix outputsTrain'
-    return matrix'
-
-f1 :: [[Int]] -> Int -> IO Float
-f1 confMat classIndex = do
-    -- returns f1 score of one class
-    tp' <- tp confMat classIndex
-    fp' <- fp confMat classIndex
-    fn' <- fn confMat classIndex
-    let precisionValue = fromIntegral tp' / (fromIntegral tp' + fromIntegral fp')
-    let recallValue = fromIntegral tp' / (fromIntegral tp' + fromIntegral fn')
-    let f1Value = 2 * precisionValue * recallValue / (precisionValue + recallValue)
-    return f1Value
-
-f1Macro :: [[Int]] -> IO Float
-f1Macro confMat = do
-    let numberOfClasses = length confMat
-    f1s <- mapM (f1 confMat) [0..numberOfClasses-1]
-    let f1MacroValue = sum f1s / fromIntegral (length f1s)
-    return f1MacroValue
+fn :: [[Int]] -> Int -> Int
+fn confMat classIndex = sum col - col !! classIndex
+    where
+        col = map (!! classIndex) confMat
 
 
-f1Micro :: [[Int]] -> IO Float
-f1Micro confMat = do
-    let numberOfClasses = length confMat
-    sumTps <- mapM (tp confMat) [0..numberOfClasses-1]
-    sumFps <- mapM (fp confMat) [0..numberOfClasses-1]
-    sumFns <- mapM (fn confMat) [0..numberOfClasses-1]
-    -- TPs / TPs+(0.5(FPs+FNs))
-    let micro = fromIntegral (sum sumTps) / (fromIntegral (sum sumTps) + 0.5 * (fromIntegral (sum sumFps) + fromIntegral (sum sumFns)))
-    return micro
+accuracy :: [[Int]] -> Float
+accuracy confMat = fromIntegral (sum tps) / fromIntegral numberOfElements
+    where
+        numberOfClasses = length confMat
+        tps = map (tp confMat) [0..numberOfClasses-1]
+        numberOfElements = sum (map sum confMat)
 
-f1Weighted :: [[Int]] -> IO Float
-f1Weighted confMat = do
-    let numberOfClasses = length confMat
-    f1s <- mapM (f1 confMat) [0..numberOfClasses-1]
-    let weights = map (\classIndex -> fromIntegral (length (confMat !! classIndex))) [0..numberOfClasses-1]
-    let f1WeightedValue = sum (zipWith (*) f1s weights) / sum weights
-    return f1WeightedValue
+confusionMatrix :: MLPParams -> [(Tensor, Tensor)] -> [[Int]]
+confusionMatrix model dataset = matrix'
+  where
+    outputsTrain = map (\(input, actual) -> (actual, mlpLayer model input)) dataset
+    outputsTrain' = map (\(actual, prediction) -> (actual, prediction)) outputsTrain
+    matrix = [[0 | _ <- [(0 :: Integer)..9]] | _ <- [(0 :: Integer)..9]]
+    matrix' = foldl updateMatrix matrix outputsTrain'
+    updateMatrix acc (actual, prediction) = 
+        let i = argmax (asValue actual)
+            j = argmax (asValue prediction)
+            row = acc !! i
+            row' = take j row ++ [row !! j + 1] ++ drop (j + 1) row
+            acc' = take i acc ++ [row'] ++ drop (i + 1) acc
+        in acc'
+
+-- returns f1 score of one class
+f1 :: [[Int]] -> Int -> Float
+f1 confMat classIndex = 2 * precisionValue * recallValue / (precisionValue + recallValue)
+    where 
+        tp' = tp confMat classIndex
+        fp' = fp confMat classIndex
+        fn' = fn confMat classIndex
+        precisionValue = fromIntegral tp' / (fromIntegral tp' + fromIntegral fp')
+        recallValue = fromIntegral tp' / (fromIntegral tp' + fromIntegral fn')
+
+f1Macro :: [[Int]] -> Float
+f1Macro confMat = sum f1s / fromIntegral (length f1s)
+    where
+        numberOfClasses = length confMat
+        f1s = map (f1 confMat) [0..numberOfClasses-1]
+
+-- TPs / TPs+(0.5(FPs+FNs))
+f1Micro :: [[Int]] -> Float
+f1Micro confMat = fromIntegral (sum sumTps) / (fromIntegral (sum sumTps) + 0.5 * (fromIntegral (sum sumFps) + fromIntegral (sum sumFns)))
+    where 
+        numberOfClasses = length confMat
+        sumTps = map (tp confMat) [0..numberOfClasses-1]
+        sumFps = map (fp confMat) [0..numberOfClasses-1]
+        sumFns = map (fn confMat) [0..numberOfClasses-1]
+
+
+f1Weighted :: [[Int]] -> Float
+f1Weighted confMat = sum (zipWith (*) f1s weights) / sum weights
+    where
+        numberOfClasses = length confMat
+        f1s = map (f1 confMat) [0..numberOfClasses-1]
+        weights = map (\classIndex -> fromIntegral (length (confMat !! classIndex))) [0..numberOfClasses-1]
+
 
 avarage :: [Float] -> Float
 avarage xs = sum xs / fromIntegral (length xs)
